221adfba45d3598402ed250ea708057b1a071141
Krishna Gupta <161935799+KrishnaGupta0405@users.noreply.github.com>
2025-02-02 22:32:04 +0530

    Time: 0 ms (100%), Space: 32 MB (71.49%) - LeetHub

---
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+        // using gpt
+        int left = 0, sum = 0, minLength = INT_MAX;
+        
+        for (int right = 0; right < nums.size(); right++) {
+            sum += nums[right];
+
+            while (sum >= target) {
+                minLength = min(minLength, right - left + 1);
+                sum -= nums[left++];
+            }
+        }
+        
+        return (minLength == INT_MAX) ? 0 : minLength;
+    }
+};
