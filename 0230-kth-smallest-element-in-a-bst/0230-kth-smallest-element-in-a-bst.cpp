642c4c9bc0ca8ad5a6038409619b3f15cc8d1e59
Krishna Gupta <161935799+KrishnaGupta0405@users.noreply.github.com>
2025-03-22 19:59:48 +0530

    Time: 0 ms (100%), Space: 24.5 MB (42.9%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
@@ -9,20 +9,43 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+//  Aprroach -> 1
+// #include<algorithm>
+// class Solution {
+// public:
+//     void inorder(TreeNode* root, vector<int>& arr){
+//         if (root==NULL){
+//             return;
+//         }
+//         inorder(root->left, arr);
+//         arr.push_back(root->val);
+//         inorder(root->right, arr);
+//     }
+//     int kthSmallest(TreeNode* root, int k) {
+//         vector<int> ans;
+//         inorder(root,ans);
+//         return ans[k-1];
+//     }
+// };
+
+// Approach -> 2
 #include<algorithm>
 class Solution {
 public:
-    void inorder(TreeNode* root, vector<int>& arr){
-        if (root==NULL){
-            return;
-        }
-        inorder(root->left, arr);
-        arr.push_back(root->val);
-        inorder(root->right, arr);
+int solve( TreeNode* root, int& i , int k){
+    if (!root) return -1;
+    int left = solve( root -> left, i, k);
+    if (left != -1){
+        return left;
     }
+    i++;
+    if ( i == k){
+        return root-> val;
+    }
+    return solve(root->right , i, k);
+}
     int kthSmallest(TreeNode* root, int k) {
-        vector<int> ans;
-        inorder(root,ans);
-        return ans[k-1];
+        int i=0;
+        return solve(root, i, k);
     }
 };
