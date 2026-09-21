d0d6fc643abd9366c9732ab93738949843bc35a4
Krishna Gupta <161935799+KrishnaGupta0405@users.noreply.github.com>
2026-09-21 19:33:31 +0530

    Time: 5 ms (25.74%), Space: 263.4 MB (40.95%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
-    void rotate(vector<int>& nums, int k) {
-        int n = nums.size();
-        k = k % n;  // To handle cases where k > n
-        reverse(nums.begin(), nums.end());  // Reverse the whole array
-        reverse(nums.begin(), nums.begin() + k);  // Reverse first k elements
-        reverse(nums.begin() + k, nums.end());  // Reverse the remaining elements
+    void rotate(vector<int>& arr, int k) {
+        int n = arr.size();
+        k = k %n; //Normalisizing the rotation value
+        // if array size if 5, then both 7 and 2 rotation performed seperately would give the same answer
+        reverse(arr.begin(), arr.end());
+        reverse(arr.begin(), arr.begin()+k);
+        reverse(arr.begin()+k, arr.end());
     }
 };
