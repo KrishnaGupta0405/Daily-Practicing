f52a074b6249c93c79917bcf89d2c098416f60f6
Krishna Gupta <161935799+KrishnaGupta0405@users.noreply.github.com>
2026-09-24 17:30:45 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Wave Array/wave-array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    void sortInWave(vector<int>& arr) {
+        // code here
+        int n = arr.size();
+
+        // swap adjacent elements to create 
+        // wave pattern
+        for (int i = 0; i < n - 1; i += 2){
+            swap(arr[i], arr[i + 1]);
+        }
+    }
+};
