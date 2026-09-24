c8e94c59e44cb3167c68f616a8236e3e66985467
Krishna Gupta <161935799+KrishnaGupta0405@users.noreply.github.com>
2026-09-24 17:35:54 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Two Dimensional World/two-dimensional-world.cpp
@@ -0,0 +1,10 @@
+void twoDimensional(int n, int **mat) {
+    for(int i = 0; i < n; i++){
+        
+        for(int j = 0; j < n; j++){
+            
+            cout<< mat[i][j]<< " ";
+        }
+        cout<<endl;
+    }
+}
